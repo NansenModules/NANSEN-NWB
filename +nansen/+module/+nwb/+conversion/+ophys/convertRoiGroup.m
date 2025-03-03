100886c51eac4acea4e484bd92f5b05069784be8
ehennestad <ehennestad@gmail.com>
2025-03-03 22:21:11 +0100

    Unfinished changes for smaller final file size

--- +nansen/+module/+nwb/+conversion/+ophys/convertRoiGroup.m
+++ +nansen/+module/+nwb/+conversion/+ophys/convertRoiGroup.m
@@ -1,10 +1,11 @@
-function planeSegmentation = convertRoiGroup(roiGroup, isCell, metadata)
+function planeSegmentation = convertRoiGroup(roiGroup, isCell, metadata, options)
 % convertRoiGroup - Convert roig group to plane segmentation neurodata type    
     
     arguments
         roiGroup
         isCell (:,1) logical = logical.empty
         metadata (1,1) struct = struct
+        options.MaskType = 'pixel'
     end
 
     % Todo: Loop over channels and planes of RoiGroup?
@@ -17,6 +18,9 @@ function planeSegmentation = convertRoiGroup(roiGroup, isCell, metadata)
     if ~isempty(isCell)
         roiArray = roiArray(isCell);
     end
+
+    planeSegmentation = createPixelMaskPlaneSegmentation(roiArray);
+    return
 
     % Prepare imagemasks
     numRois = numel(roiArray);
@@ -37,5 +41,54 @@ function planeSegmentation = convertRoiGroup(roiGroup, isCell, metadata)
             'description', 'image masks', ...
             'data', imageMask ...
             ) ...
+    );
+end
+
+function plane_segmentation = createPixelMaskPlaneSegmentation(roiArray)
+    
+    numRois = numel(roiArray);
+
+    xInd = cell(1,numel(roiArray));
+    yInd = cell(1,numel(roiArray));
+    w = cell(1,numel(roiArray));
+
+    for i = 1:numel(roiArray)
+        xInd{i} = roiArray(i).coordinates(:,1);
+        yInd{i} = roiArray(i).coordinates(:,2);
+        w{i} = roiArray(i).pixelweights;
+    end
+    pixel_mask_struct = struct();
+    pixel_mask_struct.x = uint32( cat(1, xInd{:}) ); % Add x coordinates to struct field x
+    pixel_mask_struct.y = uint32( cat(1, yInd{:}) ); % Add y coordinates to struct field y
+    pixel_mask_struct.weight = single( cat(1, w{:}) ); 
+    
+    % Create pixel mask vector data
+    pixel_mask = types.hdmf_common.VectorData(...
+            'data', struct2table(pixel_mask_struct), ...
+            'description', 'pixel masks');
+
+    % When creating a pixel mask, it is also necessary to specify a
+    % pixel_mask_index vector. See the documentation for ragged arrays linked
+    % above to learn more.
+    numPixelsPerRoi = zeros(numel(roiArray), 1); % Column vector
+    for iRoi = 1:numel(roiArray)
+        numPixelsPerRoi(iRoi) = numel(roiArray(iRoi).pixelweights);
+    end
+
+    pixelMaskIndex = uint32(cumsum(numPixelsPerRoi)); % Note: Use an integer 
+    % type that can accommodate the maximum value of the cumulative sum
+
+    % Create pixel_mask_index vector
+    pixelMaskIndex = types.hdmf_common.VectorIndex(...
+            'description', 'Index into pixel_mask VectorData', ...
+            'data', pixelMaskIndex, ...
+            'target', types.untyped.ObjectView(pixel_mask) );
+
+    plane_segmentation = types.core.PlaneSegmentation( ...
+        'colnames', {'pixel_mask'}, ...
+        'description', 'roi pixel position (x,y) and pixel weight', ...
+        'pixel_mask_index', pixelMaskIndex, ...
+        'pixel_mask', pixel_mask, ...
+        'id', types.hdmf_common.ElementIdentifiers('data', int64(0:numRois-1)') ...
     );
 end
--- +nansen/+module/+nwb/+conversion/+ophys/convertRoiResponses.m
+++ +nansen/+module/+nwb/+conversion/+ophys/convertRoiResponses.m
@@ -18,10 +18,37 @@ function rrs = convertRoiResponses(signalArray)
     % Todo: Get time props from timetable (utility method), i.e timestamps
     % or starting_time
 
+    numericData = signalArray.Variables';  % Transpose to get time along second dimension
+
+    chunkSize = getChunkSize(numericData, {64, 'flex'}, 10e6); % 10MB
+    maxSize = size(numericData);
+
+    dataPipeArgs = {...
+        "data", numericData, ...
+        "maxSize", maxSize, ...
+        "chunkSize", chunkSize, ...
+        "hasShuffle", true, ...
+        "compressionLevel", 3 };
+
+    data = types.untyped.DataPipe(dataPipeArgs{:});
+
     % Create roi response series
     rrs = types.core.RoiResponseSeries(...
-        'data', signalArray.Variables', ... % Transpose to get time along second dimension
+        'data', data, ...
         'data_unit', 'fluorescence intensity', ...
         'starting_time', 0, ...
         'starting_time_rate', signalArray.Properties.SampleRate );
 end
+
+function chunkSize = getChunkSize(A, dimensionConstraints, targetChunkSize)
+
+    info = whos("A");
+    elementSize = info.bytes / numel(A); % bytes per element
+
+    % Determine the target number of elements per chunk.
+    targetNumElements = targetChunkSize / elementSize;
+    
+    flexLength = round(targetNumElements / dimensionConstraints{1});
+    chunkSize = [ dimensionConstraints{1}, flexLength ];
+end
+
